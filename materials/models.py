8416d216ff35ad9502d64bb90401f9702d9af412
Skillova <skillova@yandex.ru>
2024-07-13 18:42:43 +0000

    созданы модели user & course & lesson

--- materials/models.py
+++ materials/models.py
@@ -1,3 +1,62 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Course(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Название',
+        help_text='Укажите название курса'
+    )
+    image = models.ImageField(
+        upload_to='users/avatars',
+        verbose_name='Превью ',
+        help_text='Укажите Превью'
+    )
+    description = models.TextField(
+        verbose_name='Название',
+        help_text='Укажите название курса',
+        **NULLABLE
+    )
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(
+        max_length=150,
+        verbose_name='Название',
+        help_text='Укажите название курса',
+        ** NULLABLE
+    )
+    image = models.ImageField(
+        upload_to='users/avatars',
+        verbose_name='Превью ',
+        help_text='Укажите Превью'
+    )
+    description = models.TextField(
+        verbose_name='Название',
+        help_text='Укажите название курса',
+        **NULLABLE
+    )
+    video = models.CharField(
+        max_length=250,
+        verbose_name='Ссылка на видео',
+        help_text='Укажите ссылку на видео',
+        **NULLABLE
+    )
+    course = models.ForeignKey(
+        to=Course,
+        on_delete=models.SET_NULL,
+        verbose_name='Курс',
+        help_text='Укажите курс',
+        **NULLABLE
+    )
+
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
--- users/models.py
+++ users/models.py
@@ -1,3 +1,39 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"null": True, "blank": True}
+
+
+class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(
+        unique=True,
+        verbose_name="Почта",
+        help_text="Укажите почту"
+    )
+    phone_number = models.CharField(
+        max_length=35,
+        verbose_name="Телефон",
+        help_text="Укажите номер телефона",
+        **NULLABLE
+    )
+    country = models.CharField(
+        max_length=50,
+        verbose_name="Страна",
+        help_text="Укажите страну",
+        **NULLABLE
+    )
+    avatar = models.ImageField(
+        upload_to="users/avatars",
+        verbose_name="Аватар",
+        help_text="Укажите аватар",
+        **NULLABLE
+    )
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
