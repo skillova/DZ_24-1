579e22ac640dbe1d499749b338797c64fd023270
Skillova <skillova@yandex.ru>
2024-07-14 07:59:46 +0000

    CRUD для Lesson с помощью Generic

--- materials/urls.py
+++ materials/urls.py
@@ -1,4 +1,6 @@
+from django.urls import path
 from rest_framework.routers import SimpleRouter
+from materials.views import LessonCreateApiView, LessonListApiView, LessonRetrieveApiView, LessonUpdateApiView, LessonDestroyApiView
 
 from materials.apps import MaterialsConfig
 from materials.views import CourseViewSet
@@ -9,6 +11,12 @@ router = SimpleRouter()
 router.register('', CourseViewSet)
 
 
-urlpatterns = []
+urlpatterns = [
+    path('lesson/', LessonListApiView.as_view(), name='lesson_list'),
+    path('lesson/<int:pk>/', LessonRetrieveApiView.as_view(), name='lesson_retrieve'),
+    path('lesson/create', LessonCreateApiView.as_view(), name='lesson_create'),
+    path('lesson/<int:pk>/delete', LessonDestroyApiView.as_view(), name='lesson_delete'),
+path('lesson/<int:pk>/update', LessonUpdateApiView.as_view(), name='lesson_update'),
+]
 
 urlpatterns += router.urls
