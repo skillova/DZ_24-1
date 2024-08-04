f7d348e395148d4bb44a773aca24f9555afea02b
Skillova <skillova@yandex.ru>
2024-08-04 17:37:24 +0000

    деактивация пользователя по простою.

--- users/tasks.py
+++ users/tasks.py
@@ -1,6 +1,8 @@
 import datetime
 
 from celery import shared_task
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
 from users.models import User
 
@@ -8,9 +10,10 @@ from users.models import User
 @shared_task
 def check_user_activity():
     """ Проверка активности пользователя, если более 30 дней, то user.is_active = False"""
-    users = User.objects.all()
-    date_now = datetime.date.today()
-    for user in users:
-        if date_now > user.last_login:
-            user.is_active = False
-            user.save()
+
+    user = get_user_model()
+    deadline_data = timezone.now() - datetime.timedelta(days=30)
+    # фильтр пользователей по дате -deadline_data, активный пользователь и нелогиневшемуся пользователю
+    inactive_users = user.objects.filter(last_login__lt=deadline_data, is_active=True, last_login=not None)
+    inactive_users.update(is_active=False)
+    print(f'Deactivated {inactive_users.count()} users')
